ca47a8deebe695aaa301d6319b1b67ada3cf28d2
Juan D. Rincon U <jd.rincon021@gmail.com>
2023-08-22 23:43:43 -0400

    instalacion de framework

---
+++ application/controllers/Login.php
@@ -0,0 +1,16 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+        
+    }
+}
+
+/* End of file Login.php and path \application\controllers\Login.php */
